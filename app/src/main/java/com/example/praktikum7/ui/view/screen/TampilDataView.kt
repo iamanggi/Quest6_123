f5afac12d803a139b449973aa41914d98771f210
iamanggi <anggipuspita452@gmail.com>
2024-11-21 22:08:44 +0700

    Memperbarui Tampilan

--- app/src/main/java/com/example/praktikum7/ui/view/screen/TampilDataView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/screen/TampilDataView.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum7.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,32 +37,52 @@ fun TampilDataView(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.primary)),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Column(
-            modifier = Modifier
                 .fillMaxSize()
-                .background(Color.White)
-                .padding(16.dp),
+                .background(color = colorResource(id = R.color.primary))
+                ,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(80.dp))
+        Spacer(modifier = Modifier.padding(24.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logo_umy
+                ),
+                contentDescription = "", modifier = Modifier.size(60.dp),
+            )
+
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red, fontSize = 16.sp, fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red
+                )
+            }
+        }
+            Spacer(modifier = Modifier.height(30.dp))
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Color.LightGray, shape = RoundedCornerShape(15.dp))
-                    .padding(16.dp)
+                    .background(
+                        color = Color.White, shape = RoundedCornerShape(
+                            topEnd = 15.dp,
+                            topStart = 15.dp
+                        )
+                    )
+                    .fillMaxSize()
             ) {
                 Column(
+
                     horizontalAlignment = Alignment.Start
                 ) {
                     Row(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier.fillMaxWidth().padding(18.dp),
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Column {
+                        Column(modifier = Modifier.padding(top = 20.dp)) {
                             Text(
                                 text = "Nim:",
                                 fontWeight = FontWeight.Bold,
@@ -80,7 +103,7 @@ fun TampilDataView(
                                 fontSize = 16.sp
                             )
                         }
-                        Column {
+                        Column(modifier = Modifier.padding(top = 20.dp))  {
                             Text(
                                 text = uiState.email,
                                 fontWeight = FontWeight.Bold,
@@ -90,7 +113,7 @@ fun TampilDataView(
                     }
                     Spacer(modifier = Modifier.padding(12.dp))
                     Column(
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 18.dp)
                     ) {
                         Text(
                             text = "Matakuliah yang diambil:",
@@ -114,26 +137,26 @@ fun TampilDataView(
                             fontSize = 16.sp
                         )
                     }
-                }
-            }
-            Spacer(modifier = Modifier.padding(12.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button(onClick = { onBackButtonClicked() }) {
-                    Text(
-                        text = "Kembali", fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium,
-                    )
-                }
-                Button(onClick = { onResetButtonClicked() }) {
-                    Text(
-                        text = "Reset",fontSize = 16.sp,
-                        fontWeight = FontWeight.Medium
-                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Button(onClick = { onBackButtonClicked() }) {
+                            Text(
+                                text = "Kembali", fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium,
+                            )
+                        }
+                        Button(onClick = { onResetButtonClicked() }) {
+                            Text(
+                                text = "Reset", fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
+
