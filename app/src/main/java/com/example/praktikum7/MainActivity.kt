d90fe1447e7190b7c5956d58fa2d1ac08b6a139f
iamanggi <anggipuspita452@gmail.com>
2024-11-21 21:41:12 +0700

    Memanggil PengelolaanHalaman/ Navigator pada main activity

--- app/src/main/java/com/example/praktikum7/MainActivity.kt
+++ app/src/main/java/com/example/praktikum7/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikum7.navigation.PengelolaanHalaman
 import com.example.praktikum7.ui.theme.Praktikum7Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum7Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaanHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
