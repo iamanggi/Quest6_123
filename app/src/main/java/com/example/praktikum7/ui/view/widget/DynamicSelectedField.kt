65018fb4e67049383ad42298737a1413b6f8c3bb
iamanggi <anggipuspita452@gmail.com>
2024-11-21 18:38:33 +0700

    Membuat komponen DynamicSelectedTextField untuk menampilkan dropdown pilihan dengan label dan menangani perubahan nilai

--- app/src/main/java/com/example/praktikum7/ui/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/widget/DynamicSelectedField.kt
@@ -1,2 +1,19 @@
 package com.example.praktikum7.ui.view.widget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+}
