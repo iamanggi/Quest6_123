1d5bb9eb61eaeb9c3e08ea94934f1af2d10c69cd
iamanggi <anggipuspita452@gmail.com>
2024-11-21 18:26:58 +0700

    Menambahkan RencanaStudyView untuk input mata kuliah dan kelas

--- app/src/main/java/com/example/praktikum7/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/screen/RencanaStudiView.kt
@@ -1,2 +1,22 @@
 package com.example.praktikum7.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.praktikum7.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+    var chosenDropdown by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+}
+
