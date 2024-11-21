3d0aae359007ba8d328f50f0963395aff09f2a3f
iamanggi <anggipuspita452@gmail.com>
2024-11-21 18:24:14 +0700

    Menambahkan form input mahasiswa dengan field NIM, nama, dan email, serta tombol 'Kembali' dan 'Simpan' pada MahasiswaFormView

--- app/src/main/java/com/example/praktikum7/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/screen/MahasiswaFormView.kt
@@ -2,12 +2,24 @@ package com.example.praktikum7.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,9 +32,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikum7.R
+
 
 @Composable
 fun MahasiswaFormView(
@@ -61,6 +76,113 @@ fun MahasiswaFormView(
                     text = "Unggul dan Islami",
                     color = Color.Red
                 )
+            }
+        }
+        Spacer(modifier = Modifier.padding(top = 30.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White, shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(18.dp).padding(top = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Text(
+                    text = "Masukkan Data Kamu",
+                    fontWeight = FontWeight.Bold, fontSize = 20.sp
+                )
+
+                Text(
+                    text = "Isi sesuai data yang kamu daftarkan",
+                    fontWeight = FontWeight.Light
+                )
+
+                Spacer(modifier = Modifier.padding(14.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
+                    value = nim,
+                    onValueChange = { nim = it },
+                    label = {
+                        Text(text = "Nomor Induk Mahasiswa")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp)
+                )
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
+                    value = nama,
+                    onValueChange = { nama = it },
+                    label = {
+                        Text(text = "Nama Mahasiswa")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp)
+                )
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
+                    value = email,
+                    onValueChange = { email = it },
+                    label = {
+                        Text(text = "Email Mahasiswa")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Email,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp)
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                )
+                {
+                    Button(onClick = { onBackButtonClicked() }) {
+                        Text(text = "Kembali", fontSize = 16.sp)
+                    }
+                    Button(onClick = { onSubmitButtonClicked(listData) }) {
+                        Text(text = "Simpan", fontSize = 16.sp)
+                    }
+                }
             }
         }
 
