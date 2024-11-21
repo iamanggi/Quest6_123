247db56c856e2394184a9b46157c1e5aa78668ad
iamanggi <anggipuspita452@gmail.com>
2024-11-21 21:36:48 +0700

    Implementasi navigasi dengan `NavHost`. Halaman awal `Splash` navigasi ke `Mahasiswa`

--- app/src/main/java/com/example/praktikum7/navigation/PengelolaanHalaman.kt
+++ app/src/main/java/com/example/praktikum7/navigation/PengelolaanHalaman.kt
@@ -1,5 +1,19 @@
 package com.example.praktikum7.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.example.praktikum7.ui.view.viewmodel.MahasiswaViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum7.ui.view.screen.SplashView
+import com.example.praktikum7.ui.view.viewmodel.RencanaStudyViewModel
+
 
 enum class Halaman{
     Splash,
@@ -8,3 +22,30 @@ enum class Halaman{
     Tampil
 }
 
+
+
+@Composable
+fun PengelolaanHalaman(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val uiState by mahasiswaViewModel.statusUI.collectAsState()
+    val krsState by krsViewModel.krsStateUi.collectAsState()
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ) {
+        composable(
+            route = Halaman.Splash.name
+        ) {
+            SplashView(
+                onMulaiButton = {
+                    navController.navigate(Halaman.Mahasiswa.name)
+                }
+            )
+        }
+    }
+}
