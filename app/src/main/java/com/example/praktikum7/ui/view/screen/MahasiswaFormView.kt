505995a68b5da304d4bfc165c563231cced8cc13
iamanggi <anggipuspita452@gmail.com>
2024-11-21 18:18:56 +0700

    Menambahkan tampilan MahasiswaFormView dengan header dan logo Universitas Muhammadiyah Yogyakarta

--- app/src/main/java/com/example/praktikum7/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/screen/MahasiswaFormView.kt
@@ -1,21 +1,68 @@
 package com.example.praktikum7.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum7.R
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-){
+) {
     var nim by remember { mutableStateOf("") }
     var nama by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
 
     val listData: MutableList<String> = mutableListOf(nim, nama, email)
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Spacer(modifier = Modifier.padding(24.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logo_umy
+                ),
+                contentDescription = "", modifier = Modifier.size(60.dp),
+            )
 
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red, fontSize = 16.sp, fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red
+                )
+            }
+        }
+
+    }
 }
