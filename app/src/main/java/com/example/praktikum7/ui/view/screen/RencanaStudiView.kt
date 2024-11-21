d3634096445ec862f17c08f8c188711cf53e801b
iamanggi <anggipuspita452@gmail.com>
2024-11-21 18:30:19 +0700

    Menambahkan tampilan mahasiswa dengan nama, NIM, logo UMY, dan ikon notifikasi

--- app/src/main/java/com/example/praktikum7/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/praktikum7/ui/view/screen/RencanaStudiView.kt
@@ -1,10 +1,33 @@
 package com.example.praktikum7.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum7.R
 import com.example.praktikum7.model.Mahasiswa
 
 @Composable
@@ -18,5 +41,45 @@ fun RencanaStudyView(
     var pilihanKelas by remember { mutableStateOf("") }
 
     val listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column(
+        modifier = Modifier.fillMaxSize().background(color = colorResource(id = R.color.primary))
+    ) {
+        Spacer(modifier = Modifier.padding(20.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.logo_umy),
+                contentDescription = "", modifier = Modifier.size(60.dp)
+            )
+
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }
+            Box() {
+                Icon(
+                    imageVector = Icons.Outlined.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
+
 
